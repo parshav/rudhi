305dccf79152a6d578e0b5377392f528b7279250
pv <parshav@live.com>
2020-08-04 18:51:06 -0500

    Added chrono and default structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+extern crate chrono;
+
 fn main() {
-    println!("Hello, world!");
+
+	// load existing -> all in home directory
+	// states -> empty, some
+	// dummy data  for now
+
+}
+
+fn dummy_data() -> [Routine; 1] {
+
+	return [
+		Routine {
+			name: String::from("Running"),
+			last_done: String::from("30/07/2020")
+		}
+	]
+}
+
+struct Routine {
+	name: String,
+	last_done: String // should be date
+	// maybe an array of last done or notes for each time done
+}
+
+struct Todo {
+	name: String,
+	added: String // should be date
+}
+
+struct Thought {
+	name: String,
+	description: String
 }
