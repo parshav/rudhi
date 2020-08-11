b1dc35089c56026216b769f70e408f3c02d77229
pv <parshav@live.com>
2020-08-10 20:49:06 -0500

    Separated routine to different file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 extern crate chrono;
 
-use chrono::prelude::*;
-use std::fmt;
+mod routine;
+
+use routine::Routine;
 
 fn main() {
 
@@ -9,9 +10,8 @@ fn main() {
 	// states -> empty, some
 
 	
-	let routines = dummy_routines_data();
+	let routines = routine::dummy_routines_data();
 	print_routines(&routines);
-
 }
 
 fn print_routines(routines: &[Routine]) {
@@ -20,36 +20,6 @@ fn print_routines(routines: &[Routine]) {
 	});
 }
 
-fn dummy_routines_data() -> [Routine; 3] {
-
-	return [
-		Routine {
-			name: String::from("Running"),
-			last_done: Local.ymd(2020, 7, 30)
-		},
-		Routine {
-			name: String::from("Reading"),
-			last_done: Local.ymd(2020, 7, 31)
-		},
-		Routine {
-			name: String::from("Coding"),
-			last_done: Local.ymd(2020, 8, 3)
-		}
-	]
-}
-
-struct Routine {
-	name: String,
-	last_done: Date<Local>
-	// maybe an array of last done or notes for each time done
-}
-
-impl fmt::Display for Routine {
-
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-  		write!(f, "{} last done on {}", self.name, self.last_done)
-    }
-}
 
 /* Will be done later
 struct Todo {
---
+++ src/routine.rs
@@ -0,0 +1,34 @@
+use chrono::prelude::*;
+use std::fmt;
+
+pub struct Routine {
+	pub name: String,
+	pub last_done: Date<Local>
+	// maybe an array of last done or notes for each time done
+}
+
+impl fmt::Display for Routine {
+
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+  		write!(f, "{} last done on {}", self.name, self.last_done)
+    }
+}
+
+// mock
+pub fn dummy_routines_data() -> [Routine; 3] {
+
+	return [
+		Routine {
+			name: String::from("Running"),
+			last_done: Local.ymd(2020, 7, 30)
+		},
+		Routine {
+			name: String::from("Reading"),
+			last_done: Local.ymd(2020, 7, 31)
+		},
+		Routine {
+			name: String::from("Coding"),
+			last_done: Local.ymd(2020, 8, 3)
+		}
+	]
+}
